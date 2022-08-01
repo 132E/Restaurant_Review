97e2cbbc09793ccb3c61acc32be2e327f0de013e
E132 <107452563+132E@users.noreply.github.com>
2022-08-01 10:24:33 -0500

    get a single route

--- server.js
+++ server.js
@@ -7,12 +7,26 @@ const app = express();
 const { Restaurant } = require("./models");
 
 // MIDDLEWARE
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(`${__dirname}/client/build`));
 
 // ROUTES
+// GET all restaurants
 app.get("/restaurants", async (req, res) => {
   let restaurants = await Restaurant.find({});
   res.send(restaurants);
+});
+// create a restaurant
+app.post("/restaurants", async (req, res) => {
+  let createdRestaurant = await Restaurant.create(req.body);
+  res.send(createdRestaurant);
+});
+
+// GET a single restaurant
+app.get("/restaurants/:id", async (req, res) => {
+  let foundRrestaurant = await Restaurant.findById(req.params.id);
+  res.send(foundRrestaurant);
 });
 
 app.get("/*", (req, res) => {
