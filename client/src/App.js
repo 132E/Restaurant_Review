d836ff8b3caae34eef10d86e1e6bb7a7192c5fe4
E132 <107452563+132E@users.noreply.github.com>
2022-08-02 10:01:14 -0500

    frontend set up

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import Nav from "./components/Nav";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <Nav />
+      </header>
+      <main>
+        <Routes>
+          <Route />
+          <Route />
+          <Route />
+          <Route />
+          <Route />
+          <Route />
+        </Routes>
+      </main>
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import { NavLink } from "react-router-dom";
+const Nav = () => {
+    return(
+        <nav>
+            <h1>iRate</h1>
+            <div>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/restaurants">Restaurants</NavLink>
+            <NavLink to="/add">Add Restaurants</NavLink>
+            </div>
+
+        </nav>
+    );
+}
+
+export default Nav;
